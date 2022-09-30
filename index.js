352a267682809adc22ea7cf007abe49047975a4d
Ben <84840243+behnam-fathollahi@users.noreply.github.com>
2022-09-30 21:29:37 +1000

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ function clock(){
         h = h - 12;
         ampm = "PM";
     }
+     h = h<10 ? "0" + h : h;
+    m = m<10 ? "0" + m : m;
+    s = s<10 ? "0" + s : s;
+
 
     hourEl.innerText = h;
     minuteEl.innerText = m;
